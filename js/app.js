fe26a147103390cafefcb29cf642183e047844fa
Tim Chase <tchase4544@gmail.com>
2017-06-21 09:59:48 -0400

    setting up still

--- js/app.js
+++ js/app.js
@@ -1,5 +1,64 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+	let gramsData = [{
+		photo_url: "http://placecage.com/100/100",
+		author: "Nic Cage",
+		body: "Filler test that is filler"
+	},{
+		photo_url: "http://placecage.com/200/200",
+		author: "Nicolas Cage",
+		body: "sflk adsfj aflakdfdfjf ffj"
+	},{
+		photo_url: "http://placecage.com/300/300",
+		author: "N C",
+		body: "ksjksfiaf fafkafa fa faf fdfsdf"
+	}]
+  angular.module("wdinstagram", ["ui.router"])
+  .config([
+  	'$stateProvider',
+  	"$locationProvider",
+  	Router
+  	])
+  // .controller("InstaIndexController", [InstaIndexCtrlFun])
+  .controller('InstaIndexController', [function(){
+  	this.posts = gramsData
+  }])
+  .controller('InstaShowController', [
+  	'$state',
+  	'$stateParams',
+  	 InstaShowCtrlFun
+  ])
+  .controller('InstaNewController', [InstaNewCtrlFun])
+
+function Router($stateProvider,$locationProvider){
+	$locationProvider.html5Mode(true)
+	$stateProvider
+	.state("instaIndex",{
+		url: "/",
+		controller: "InstaIndexController",
+		controllerAs: "vm",
+		templateUrl: "js/ng-views/index.html"
+	})
+	.state("instaNew",{
+		url: "/new",
+		controller: "InstaNewController",
+		controllerAs: "VMnew",
+		templateUrl: "js/ng-views/new.html"
+	})
+	.state("instaShow",{
+		url: "/:id",
+		controller: "InstaShowController",
+		controllerAs: "VMshow",
+		templateUrl: "js/ng-views/show.html"
+	})
+}
+	function InstaShowCtrlFun($state,$stateParams){
+		this.post = gramsData[$stateParams.id]
+	}
+
+	function InstaNewCtrlFun() {
+		// body...
+	}
+	
 })();
