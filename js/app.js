e1db9b8cb140300440b64876305421861ac3b935
Tim Chase <tchase4544@gmail.com>
2017-06-21 20:45:38 -0400

    crud finished

--- js/app.js
+++ js/app.js
@@ -1,35 +1,44 @@
 "use strict";
 
 (function(){
-	let gramsData = [{
-		photo_url: "http://placecage.com/100/100",
-		author: "Nic Cage",
-		body: "Filler test that is filler"
-	},{
-		photo_url: "http://placecage.com/200/200",
-		author: "Nicolas Cage",
-		body: "sflk adsfj aflakdfdfjf ffj"
-	},{
-		photo_url: "http://placecage.com/300/300",
-		author: "N C",
-		body: "ksjksfiaf fafkafa fa faf fdfsdf"
-	}]
-  angular.module("wdinstagram", ["ui.router"])
+	// let gramsData = [{
+	// 	photo_url: "http://placecage.com/100/100",
+	// 	author: "Nic Cage",
+	// 	body: "Filler test that is filler"
+	// },{
+	// 	photo_url: "http://placecage.com/200/200",
+	// 	author: "Nicolas Cage",
+	// 	body: "sflk adsfj aflakdfdfjf ffj"
+	// },{
+	// 	photo_url: "http://placecage.com/300/300",
+	// 	author: "N C",
+	// 	body: "ksjksfiaf fafkafa fa faf fdfsdf"
+// }];
+  angular.module("wdinstagram", ["ui.router","ngResource"])
   .config([
-  	'$stateProvider',
-  	"$locationProvider",
-  	Router
+  		"$stateProvider",
+  		"$locationProvider",
+  		Router
   	])
-  // .controller("InstaIndexController", [InstaIndexCtrlFun])
-  .controller('InstaIndexController', [function(){
-  	this.posts = gramsData
-  }])
-  .controller('InstaShowController', [
-  	'$state',
-  	'$stateParams',
-  	 InstaShowCtrlFun
+  .controller("InstaIndexController", [
+  		"InstaFactory",
+  		InstaIndexCtrlFun
+  	])
+  .controller("InstaShowController", [
+  		"$state",
+  		"$stateParams",
+  		"InstaFactory",
+  		 InstaShowCtrlFun
   ])
-  .controller('InstaNewController', [InstaNewCtrlFun])
+  .controller("InstaNewController", [
+  		"InstaFactory",
+  		"$state",
+  		InstaNewCtrlFun
+  	])
+  .factory("InstaFactory", [
+  		"$resource",
+  		InstaFactoryFun
+  	])
 
 function Router($stateProvider,$locationProvider){
 	$locationProvider.html5Mode(true)
@@ -43,22 +52,50 @@ function Router($stateProvider,$locationProvider){
 	.state("instaNew",{
 		url: "/new",
 		controller: "InstaNewController",
-		controllerAs: "VMnew",
+		controllerAs: "vmNew",
 		templateUrl: "js/ng-views/new.html"
 	})
 	.state("instaShow",{
 		url: "/:id",
 		controller: "InstaShowController",
-		controllerAs: "VMshow",
+		controllerAs: "vmShow",
 		templateUrl: "js/ng-views/show.html"
 	})
 }
-	function InstaShowCtrlFun($state,$stateParams){
-		this.post = gramsData[$stateParams.id]
+	function InstaIndexCtrlFun(InstaFactory){
+  		this.posts = InstaFactory.query()
+  		// console.log(this.posts)
+  	}
+
+  	function InstaNewCtrlFun(InstaFactory,$state){
+  		this.post = new InstaFactory();
+
+  		this.create = function(){
+  			// console.log("function fired")
+  			this.post.$save().then(function(){
+  				$state.go("instaIndex")
+  			})
+  		}
+  	}
+
+	function InstaShowCtrlFun($state,$stateParams,InstaFactory){
+		this.post = InstaFactory.get({id: $stateParams.id})
+
+		this.editPost = function(){
+			this.post.$update({id: $stateParams.id}).then(function(){
+				$state.go("instaIndex")
+			})
+		}
+		this.deletePost = function(){
+			this.post.$delete({id: $stateParams.id}).then(function(){
+				$state.go("instaIndex")
+			})
+		}
 	}
 
-	function InstaNewCtrlFun() {
-		// body...
+	function InstaFactoryFun($resource){
+		return $resource("http://localhost:3000/entries/:id",{},{
+			update: {method: 'PUT'}
+		})
 	}
-	
 })();
